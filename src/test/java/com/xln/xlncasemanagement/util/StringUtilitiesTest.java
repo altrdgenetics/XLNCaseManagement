6859c75939465d3847a844283887c5f2225b35e8
altrdgenetics <tofudeliveryae86@hotmail.com>
2017-10-10 11:33:52 -0400

    finished up the stringutilities tests

--- src/test/java/com/xln/xlncasemanagement/util/StringUtilitiesTest.java
+++ src/test/java/com/xln/xlncasemanagement/util/StringUtilitiesTest.java
@@ -155,205 +155,335 @@ public class StringUtilitiesTest {
         };
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     /**
      * Test of buildUserNameWithUserName method, of class StringUtilities.
+     * @param inputPerson
+     * @param expResult
      */
     @Test
-    public void testBuildUserNameWithUserName() {
+    @Parameters(method = "parametersForTestBuildUserNameWithUserName")
+    public void testBuildUserNameWithUserName(UserModel inputPerson, String expResult) {
         System.out.println("buildUserNameWithUserName");
-        UserModel item = null;
-        String expResult = "";
-        String result = StringUtilities.buildUserNameWithUserName(item);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, StringUtilities.buildUserNameWithUserName(inputPerson));
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private Object[] parametersForTestBuildUserNameWithUserName() {
+        UserModel user1 = new UserModel();
+        user1.setFirstName("John");
+        user1.setMiddleInitial(null);
+        user1.setLastName("Doe");
+        user1.setUsername(null);
+        
+        UserModel user2 = new UserModel();
+        user2.setFirstName("John");
+        user2.setMiddleInitial("");
+        user2.setLastName("Doe");
+        user2.setUsername("john.doe");
+        
+        UserModel user3 = new UserModel();
+        user3.setFirstName(null);
+        user3.setMiddleInitial(null);
+        user3.setLastName("Doe");
+        user3.setUsername("");
+        
+        UserModel user4 = new UserModel();
+        user4.setFirstName("John");
+        user4.setMiddleInitial("M");
+        user4.setLastName("Doe");
+        user4.setUsername("john.doe");
+        
+        UserModel user5 = new UserModel();
+        user5.setFirstName("");
+        user5.setMiddleInitial("");
+        user5.setLastName("Doe");
+        user5.setUsername("john.doe");
+        
+        return new Object[]{
+            new Object[]{user1, "John Doe"},
+            new Object[]{user2, "John Doe (john.doe)"},
+            new Object[]{user3, "Doe"},
+            new Object[]{user4, "John M. Doe (john.doe)"},
+            new Object[]{user3, "Doe"}
+        };
+    }
     
     /**
      * Test of buildUsersName method, of class StringUtilities.
+     * @param inputPerson
+     * @param expResult
      */
     @Test
-    public void testBuildUsersName() {
+    @Parameters(method = "parametersForTestBuildUsersName")
+    public void testBuildUsersName(UserModel inputPerson, String expResult) {
         System.out.println("buildUsersName");
-        UserModel item = null;
-        String expResult = "";
-        String result = StringUtilities.buildUsersName(item);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, StringUtilities.buildUsersName(inputPerson));
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private Object[] parametersForTestBuildUsersName() {
+        UserModel user1 = new UserModel();
+        user1.setFirstName("John");
+        user1.setMiddleInitial(null);
+        user1.setLastName("Doe");
+        user1.setUsername(null);
+        
+        UserModel user2 = new UserModel();
+        user2.setFirstName("John");
+        user2.setMiddleInitial("");
+        user2.setLastName("Doe");
+        user2.setUsername("john.doe");
+        
+        UserModel user3 = new UserModel();
+        user3.setFirstName(null);
+        user3.setMiddleInitial(null);
+        user3.setLastName("Doe");
+        user3.setUsername("");
+        
+        UserModel user4 = new UserModel();
+        user4.setFirstName("John");
+        user4.setMiddleInitial("M");
+        user4.setLastName("Doe");
+        user4.setUsername("john.doe");
+        
+        UserModel user5 = new UserModel();
+        user5.setFirstName("");
+        user5.setMiddleInitial("");
+        user5.setLastName("Doe");
+        user5.setUsername("john.doe");
+        
+        return new Object[]{
+            new Object[]{user1, "John Doe"},
+            new Object[]{user2, "John Doe"},
+            new Object[]{user3, "Doe"},
+            new Object[]{user4, "John M. Doe"},
+            new Object[]{user3, "Doe"}
+        };
+    }
+        
     /**
      * Test of buildName method, of class StringUtilities.
+     * @param first
+     * @param middle
+     * @param last
+     * @param expResult
      */
     @Test
-    public void testBuildName() {
+    @Parameters(method = "parametersForTestBuildName")
+    public void testBuildName(String first, String middle, String last, String expResult) {
         System.out.println("buildName");
-        String first = "";
-        String middle = "";
-        String last = "";
-        String expResult = "";
-        String result = StringUtilities.buildName(first, middle, last);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, StringUtilities.buildName(first, middle, last));
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private Object[] parametersForTestBuildName() {
+        return new Object[]{
+            new Object[]{"John", null, "Doe", "John Doe"},
+            new Object[]{"John", "", "Doe", "John Doe"},
+            new Object[]{"John", " ", "Doe", "John Doe"},
+            new Object[]{null, null, "Doe", "Doe"},
+            new Object[]{"John", "M", "Doe", "John M. Doe"},
+            new Object[]{"", "", "Doe", "Doe"}
+        };
+    }
     
     /**
      * Test of buildTableAddressBlock method, of class StringUtilities.
+     * @param person
+     * @param expResult
      */
     @Test
-    public void testBuildTableAddressBlock() {
+    @Parameters(method = "parametersForTestTableAddressBlock")
+    public void testBuildTableAddressBlock(PartyModel person, String expResult) {
         System.out.println("buildTableAddressBlock");
-        PartyModel item = null;
-        String expResult = "";
-        String result = StringUtilities.buildTableAddressBlock(item);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, StringUtilities.buildTableAddressBlock(person));
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private Object[] parametersForTestTableAddressBlock() {
+        PartyModel test1 = new PartyModel();
+        test1.setPrefix(null);
+        test1.setFirstName("John");
+        test1.setMiddleInitial(null);
+        test1.setLastName("Doe");
+        test1.setAddressOne(null);
+        test1.setAddressTwo("123 High St");
+        test1.setAddressThree("");
+        test1.setCity("Columbus");
+        test1.setState("OH");
+        test1.setZip("43215");
+        
+        PartyModel test2 = new PartyModel();
+        test2.setPrefix("");
+        test2.setFirstName("John");
+        test2.setMiddleInitial("");
+        test2.setLastName("Doe");
+        test2.setAddressOne("123 High St");
+        test2.setAddressTwo(null);
+        test2.setAddressThree("");
+        test2.setCity("Columbus");
+        test2.setState(null);
+        test2.setZip("43215");
+        
+        PartyModel test3 = new PartyModel();
+        test3.setPrefix("Mr.");
+        test3.setFirstName("John");
+        test3.setMiddleInitial(null);
+        test3.setLastName("Doe");
+        test3.setAddressOne("ATTN: Mrs. Doe");
+        test3.setAddressTwo("123 High St");
+        test3.setAddressThree("Apt 3");
+        test3.setCity("Columbus");
+        test3.setState("OH");
+        test3.setZip("43215");
+        
+        PartyModel test4 = new PartyModel();
+        test4.setPrefix(null);
+        test4.setFirstName("John");
+        test4.setMiddleInitial("M");
+        test4.setLastName("Doe");
+        test4.setAddressOne("");
+        test4.setAddressTwo("");
+        test4.setAddressThree("");
+        test4.setCity("");
+        test4.setState("OH");
+        test4.setZip("");
+        
+        return new Object[]{
+            new Object[]{test1, "123 High St, Columbus, OH 43215"},
+            new Object[]{test2, "123 High St, Columbus 43215"},
+            new Object[]{test3, "ATTN: Mrs. Doe, 123 High St, Apt 3, Columbus, OH 43215"},
+            new Object[]{test4, "OH"}
+        };
+    }
     
     /**
      * Test of buildAddressBlockWithLineBreaks method, of class StringUtilities.
+     * @param person
+     * @param expResult
      */
     @Test
-    public void testBuildAddressBlockWithLineBreaks_CompanyModel() {
+    @Parameters(method = "parametersForTestBuildAddressBlockWithLineBreaks_CompanyModel")
+    public void testBuildAddressBlockWithLineBreaks_CompanyModel(CompanyModel person, String expResult) {
         System.out.println("buildAddressBlockWithLineBreaks");
-        CompanyModel item = null;
-        String expResult = "";
-        String result = StringUtilities.buildAddressBlockWithLineBreaks(item);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, StringUtilities.buildAddressBlockWithLineBreaks(person));
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private Object[] parametersForTestBuildAddressBlockWithLineBreaks_CompanyModel() {
+        CompanyModel test1 = new CompanyModel();
+        test1.setName("XLN Systems");
+        test1.setAddressOne(null);
+        test1.setAddressTwo("123 High St");
+        test1.setAddressThree("");
+        test1.setCity("Columbus");
+        test1.setState("OH");
+        test1.setZip("43215");
+        
+        CompanyModel test2 = new CompanyModel();
+        test2.setName(null);
+        test2.setAddressOne("123 High St");
+        test2.setAddressTwo(null);
+        test2.setAddressThree("");
+        test2.setCity("Columbus");
+        test2.setState(null);
+        test2.setZip("43215");
+        
+        CompanyModel test3 = new CompanyModel();
+        test3.setName("XLN Systems");
+        test3.setAddressOne("ATTN: Mrs. Doe");
+        test3.setAddressTwo("123 High St");
+        test3.setAddressThree("Apt 3");
+        test3.setCity("Columbus");
+        test3.setState("OH");
+        test3.setZip("43215");
+        
+        CompanyModel test4 = new CompanyModel();
+        test4.setName("");
+        test4.setAddressOne("");
+        test4.setAddressTwo("");
+        test4.setAddressThree("");
+        test4.setCity("");
+        test4.setState("OH");
+        test4.setZip("");
+        
+        return new Object[]{
+            new Object[]{test1, "XLN Systems" + System.lineSeparator() + "123 High St" 
+                + System.lineSeparator() + "Columbus, OH 43215"},
+            new Object[]{test2, "123 High St" + System.lineSeparator() + "Columbus 43215"},
+            new Object[]{test3, "XLN Systems" + System.lineSeparator() 
+                + "ATTN: Mrs. Doe" + System.lineSeparator() 
+                + "123 High St" + System.lineSeparator() + "Apt 3" 
+                + System.lineSeparator() + "Columbus, OH 43215"},
+            new Object[]{test4, "OH"}
+        };
+    }   
     
     /**
      * Test of buildAddressBlockWithLineBreaks method, of class StringUtilities.
+     * @param person
+     * @param expResult
      */
     @Test
-    public void testBuildAddressBlockWithLineBreaks_PartyModel() {
+    @Parameters(method = "parametersForTestBuildAddressBlockWithLineBreaks_PartyModel")
+    public void testBuildAddressBlockWithLineBreaks_PartyModel(PartyModel person, String expResult) {
         System.out.println("buildAddressBlockWithLineBreaks");
-        PartyModel item = null;
-        String expResult = "";
-        String result = StringUtilities.buildAddressBlockWithLineBreaks(item);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, StringUtilities.buildAddressBlockWithLineBreaks(person));
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private Object[] parametersForTestBuildAddressBlockWithLineBreaks_PartyModel() {
+        PartyModel test1 = new PartyModel();
+        test1.setPrefix(null);
+        test1.setFirstName("John");
+        test1.setMiddleInitial(null);
+        test1.setLastName("Doe");
+        test1.setAddressOne(null);
+        test1.setAddressTwo("123 High St");
+        test1.setAddressThree("");
+        test1.setCity("Columbus");
+        test1.setState("OH");
+        test1.setZip("43215");
+        
+        PartyModel test2 = new PartyModel();
+        test2.setPrefix("");
+        test2.setFirstName("John");
+        test2.setMiddleInitial("");
+        test2.setLastName("Doe");
+        test2.setAddressOne("123 High St");
+        test2.setAddressTwo(null);
+        test2.setAddressThree("");
+        test2.setCity("Columbus");
+        test2.setState(null);
+        test2.setZip("43215");
+        
+        PartyModel test3 = new PartyModel();
+        test3.setPrefix("Mr.");
+        test3.setFirstName("John");
+        test3.setMiddleInitial(null);
+        test3.setLastName("Doe");
+        test3.setAddressOne("ATTN: Mrs. Doe");
+        test3.setAddressTwo("123 High St");
+        test3.setAddressThree("Apt 3");
+        test3.setCity("Columbus");
+        test3.setState("OH");
+        test3.setZip("43215");
+        
+        PartyModel test4 = new PartyModel();
+        test4.setPrefix(null);
+        test4.setFirstName("John");
+        test4.setMiddleInitial("M");
+        test4.setLastName("Doe");
+        test4.setAddressOne("");
+        test4.setAddressTwo("");
+        test4.setAddressThree("");
+        test4.setCity("");
+        test4.setState("OH");
+        test4.setZip("");
+        
+        return new Object[]{
+            new Object[]{test1, "John Doe" + System.lineSeparator() + "123 High St" 
+                + System.lineSeparator() + "Columbus, OH 43215"},
+            new Object[]{test2, "John Doe" + System.lineSeparator() + "123 High St" 
+                + System.lineSeparator() + "Columbus 43215"},
+            new Object[]{test3, "Mr. John Doe" + System.lineSeparator() + "ATTN: Mrs. Doe" 
+                + System.lineSeparator() + "123 High St" + System.lineSeparator() 
+                + "Apt 3" + System.lineSeparator() + "Columbus, OH 43215"},
+            new Object[]{test4, "John M. Doe" + System.lineSeparator() + "OH"}
+        };
+    }    
 }
